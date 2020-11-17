e5d71c7912907eab2042f4efc2ddc06abd701722
Hyun-Jun Kim <guswns9865@naver.com>
2020-11-17 20:51:43 +0900

    my mini project

---
+++ Pyo_Main/src/ddong/InitData.java
@@ -0,0 +1,9 @@
+package ddong;
+
+public class InitData {
+
+//	public static String ip = "192.168.1.9"; // 발표서버
+	public static String ip = "172.30.1.3"; // 자습실 서버
+	public static String id = "hr", pw = "hr";
+
+}
